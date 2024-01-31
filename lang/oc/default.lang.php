08d6fb1e3d04fe37baa991a944573357f365048f
joanluc <joanluc@users.noreply.github.com>
2024-01-31 11:30:32 +0100

    Update default.lang.php

--- lang/oc/default.lang.php
+++ lang/oc/default.lang.php
@@ -14,3 +14,17 @@ return array(
     'Close' => 'Clausir',
     "Manage cookies" => 'Gestion deths cookies',
     "This site uses cookies," => 'Notre siti utiliza deths cookies,',
+
+    // linhas que demoran de revirar dens tarteaucitron-oc-default.lang.php
+
+    'Comments managers facilitate the filing of comments and fight against spam.' => 'Los gestionaris de commentaras faciliten lo depòsit de votres commentaris e luten contre lo spam.',
+    'Support' => 'Support',
+    'Support services allow you to get in touch with the site team and help to improve it.' => "Los servicis de supòrt vos permiten d'entrar en contact d'amb l'esquipa du siti e d'ajudar tà son amelhament.",
+    'APIs' => 'APIs',
+    'APIs are used to load scripts: geolocation, search engines, translations, ...' => 'Las APIs permiten de cargar deths escripts : geolocalizacion, motors de recerca, reviradas, ...',
+    'Other' => 'Aute',
+    'Services to display web content.' => 'Servicis visant tà afitchar deth contengut web.',
+    'Mandatory cookies' => 'Cookies obligatòris',
+    'This site uses cookies necessary for its proper functioning which cannot be deactivated.' => 'Aquest siti utiliza deths cookies necessaris à son bon fonccionament que ne pdèn pas esser desactivats.',
+    'Allow' => 'Autorizar',
+);
