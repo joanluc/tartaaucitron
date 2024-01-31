3d70f423b4e95d817da43b7202256e425b2235e8
joanluc <joanluc@users.noreply.github.com>
2024-01-31 11:28:15 +0100

    Update default.lang.php

--- lang/oc/default.lang.php
+++ lang/oc/default.lang.php
@@ -1,1 +1,16 @@
+<?php
 
+return array(
+    'Hello! This site is transparent and lets you chose the 3rd party services you want to allow.' => 'Adishatz! Aquest siti joga la transparença e vos donna la causida deths servicis tiers que activar.',
+    'Please disable your adblocker to start customizing.' => 'Perfavor desactivatz lo vostre adblocker pèr començar la personalisacion.',
+    'Refresh the page' => 'Recargar la pagina',
+    'By continuing to scroll,' => 'En continuant de defilar,',
+    'If you continue to browse this website,' => 'En perseguiuant la vostra navigacion,',
+    'you are allowing all third-party services' => "acceptatz l'utiliszacion de servicis tiers podant instalar deths cookies",
+    'This site uses cookies and gives you control over what you want to activate' => 'Aquest siti utiliza deths cookies e vos donna lo contròsle sus aquò que soètatz activar',
+    'Manage services' => 'Gestion deths servicis',
+    'Personalize' => 'Personalizar',
+    'Accept all' => 'Tot acceptar',
+    'Close' => 'Clausir',
+    "Manage cookies" => 'Gestion deths cookies',
+    "This site uses cookies," => 'Notre siti utiliza deths cookies,',
